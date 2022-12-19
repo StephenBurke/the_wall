97a84bf98c921deb8ceae082e5d9f81d3dec003b
StephenBurke <burke.stephenpaul@gmail.com>
2022-12-18 20:59:21 -0700

    Possible scraper funtionality

--- wall_analysis/wall_analysis/spiders/__init__.py
+++ wall_analysis/wall_analysis/spiders/__init__.py
@@ -2,3 +2,24 @@
 #
 # Please refer to the documentation for information on how to create and manage
 # your spiders.
+import scrapy
+
+
+class LyricSpider(scrapy.Spider):
+    name = "lyric"
+
+    def start_requests(self):
+        urls = ['https://www.davemcnally.com/album-lyrics/the-wall/pink-floyd/']
+
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+
+        page = response.url.split("/")[-1]
+        filename = f'lyric-{page}.html'
+
+        with open(filename, 'wb') as f:
+            f.write(response.css('div/col-md-9').getall())
+
+        self.log(f'Saved file {filename}')
